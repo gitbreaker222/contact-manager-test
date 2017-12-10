73e09f07bbae3479588737e3687a5985c6024a80
Ruben L <ruben.labiunda@openmailbox.org>
2017-12-10 23:24:24 +0100

    configures router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 export class App {
   constructor() {
-    this.message = 'Hello World!';
+    this.message = 'Hello World!'
+  }
+
+  configureRouter(config, router) {
+    config.title = 'Contacts'
+    config.map([
+      {
+        route: '',
+        moduleId: 'no-selection',
+        title: 'Select'
+      },
+      {
+        route: 'contacts/:id',
+        moduleId: 'contact-detail',
+        name: 'contacts'
+      }
+    ])
+
+    this.router = router
   }
 }
