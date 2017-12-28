7feea5f64f2acf4d85cfd4097d00d1264c746bbd
Ruben L <ruben.labiunda@openmailbox.org>
2017-12-28 15:57:44 +0100

    imports events to list-component and subscribes

--- src/contact-list.js
+++ src/contact-list.js
@@ -1,11 +1,20 @@
+import {EventAggregator} from 'aurelia-event-aggregator'
 import {WebAPI} from './web-api'
+import {ContactUpdated, ContactViewed} from './messages'
 import {inject} from 'aurelia-framework'
 
-@inject(WebAPI)
+@inject(WebAPI, EventAggregator)
 export class ContactList {
-  constructor(api) {
+  constructor(api, events) {
     this.api = api
     this.contacts = []
+
+    events.subscribe(ContactViewed, message => this.select(message.contact))
+    events.subscribe(ContactUpdated, message => {
+      let id = message.contact.id
+      let found = this.contacts.find(contact => contact.id === id)
+      Object.assign(found, message.contact)
+    })
   }
 
   created() {
